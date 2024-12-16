dfb687fdbffeebea63bb6d961efb30629055fa89
nguyenduchoa0130 <nguyenduchoa0130@gmail.com>
2024-12-16 14:32:19 +0700

    Update hotel models

--- models/hotel.js
+++ models/hotel.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
 const hotelSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    introduction: { type: String },
+    introduction: { type: String, default: null },
     address: { type: String, required: true },
     star: { type: Number, default: 5, max: 5 },
+    ggMap: { type: String, default: null },
     images: [{ type: mongoose.Schema.Types.ObjectId, ref: 'hotelImages' }],
   },
   {
